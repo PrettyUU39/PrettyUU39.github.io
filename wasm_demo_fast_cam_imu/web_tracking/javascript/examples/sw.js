4d0b6deb6da0c34f757870c68ad0856079955832
jyl <yulin.jin@deepmirror.com>
2022-03-31 12:55:39 +0800

    add serviceworker14

--- wasm_demo_fast_cam_imu/web_tracking/javascript/examples/sw.js
+++ wasm_demo_fast_cam_imu/web_tracking/javascript/examples/sw.js
@@ -15,25 +15,25 @@ setTimeout(() => {
             msg: 'Hey, from service worker! I\'m listening to your fetch requests.',
             source: 'sw'
         })
-        console.log(clients);
     });
-    self.addEventListener('devicemotion', function(event) {
-        gyro_timestamp = (performance.now() + performance.timeOrigin) / 1000;
-        if (gyro_cnt > 0) {
-          gyro_hz = 1000 * gyro_cnt / (performance.now() - gyro_timestamp_start);
-          gyroInfo = `Gyro Timestamp: ${gyro_timestamp.toFixed(6)} ms
-                      Frequency: ${gyro_hz.toFixed(3)} Hz
-                      Data: ${event.acceleration.x.toFixed(3)}, ${event.acceleration.y.toFixed(3)}, ${event.acceleration.z.toFixed(3)}`;
-                      self.clients.matchAll().then ( (clients) => {
-                        clients[0].postMessage({
-                            msg: event.acceleration.x,
-                            source: 'sw'
-                        })
-                    })
-        } else {
-          gyro_timestamp_start = performance.now();
-        }
-        gyro_cnt += 1;
-      });    
-    
 }, 500);
+
+self.addEventListener('devicemotion', function(event) {
+    gyro_timestamp = (performance.now() + performance.timeOrigin) / 1000;
+    if (gyro_cnt > 0) {
+      gyro_hz = 1000 * gyro_cnt / (performance.now() - gyro_timestamp_start);
+      gyroInfo = `Gyro Timestamp: ${gyro_timestamp.toFixed(6)} ms
+                  Frequency: ${gyro_hz.toFixed(3)} Hz
+                  Data: ${event.acceleration.x.toFixed(3)}, ${event.acceleration.y.toFixed(3)}, ${event.acceleration.z.toFixed(3)}`;
+                  console.log(event.acceleration.x)
+                //   self.clients.matchAll().then ( (clients) => {
+                //     clients[0].postMessage({
+                //         msg: event.acceleration.x,
+                //         source: 'sw'
+                //     })
+                // })
+    } else {
+      gyro_timestamp_start = performance.now();
+    }
+    gyro_cnt += 1;
+  });    
